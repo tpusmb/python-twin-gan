ac23a5be2b162657825d222d7ef290863019238f
No-Code-Team <swan.blanc@etu.upmc.fr>
2018-09-20 17:15:40 +0200

    Working version of the image translation

--- image_translation.py
+++ image_translation.py
@@ -4,8 +4,11 @@
 from __future__ import absolute_import
 import os
 import logging.handlers
+
+import numpy as np
 import tensorflow as tf
 import util_io
+import timeit
 import cv2
 
 PYTHON_LOGGER = logging.getLogger(__name__)
@@ -25,14 +28,15 @@ FOLDER_ABSOLUTE_PATH = os.path.normpath(os.path.dirname(os.path.abspath(__file__
 
 
 class ImageTranslation(object):
+
     def __init__(self, model_path, image_hw=256, input_tensor_name="sources_ph",
                  output_tensor_name="custom_generated_t_style_source:0"):
         """
 
-        :param model_path:
-        :param image_hw:
-        :param input_tensor_name:
-        :param output_tensor_name:
+        :param model_path: (string) Absolute path to the model to load
+        :param image_hw:(int) Height and width of the output image
+        :param input_tensor_name: Name of the input layer for tensorflow
+        :param output_tensor_name: Name of the output layer for tensorflow
         """
         # Load the model
         PYTHON_LOGGER.info('Loading inference model')
@@ -47,7 +51,7 @@ class ImageTranslation(object):
                 image = tf.expand_dims(image, axis=0)
                 input_map = {input_tensor_name: image}
 
-            util_io.load_model(model_path, input_map=input_map)
+            self.load_model(model_path, input_map=input_map)
 
             # Get input and output tensors
             self.output = tf.get_default_graph().get_tensor_by_name(output_tensor_name)
@@ -55,18 +59,76 @@ class ImageTranslation(object):
     def translate(self, input_image):
         """
         Translate the input image
-        :param input_image: (ndarray) image to translate
-        :return:
+        :param input_image: (ndarray) opencv image to translate
+        :return: (ndarray) opencv image translated
         """
-        # util_io.imread(image_path, dtype=np.uint8)
-        feed_dict = {self.images_placeholder: input_image}
+        rgb_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+        feed_dict = {self.images_placeholder: rgb_image}
         output = self.sess.run(self.output, feed_dict=feed_dict)
         output = output[0] * 255.0  # Batch size == 1, range = 0~1.
-        return output
+        output = output.astype(np.uint8)
+        output_bgr = cv2.cvtColor(output, cv2.COLOR_RGB2BGR)
+        return output_bgr
+
+    # Adapted from https://github.com/davidsandberg/facenet/blob/master/src/facenet.py
+    def load_model(self, model, input_map=None):
+        """
+        Loads a tensorflow model and restore the variables to the default session.
+        :param model: (string) absolute path to the model
+        :param input_map: (Dictionary) empty input for the network
+        """
+        # Check if the model is a model directory (containing a metagraph and a checkpoint file)
+        #  or if it is a protobuf file with a frozen graph
+        model_exp = os.path.expanduser(model)
+        if os.path.isfile(model_exp):
+            PYTHON_LOGGER.info('Model filename: %s' % model_exp)
+            with tf.gfile.FastGFile(model_exp, 'rb') as f:
+                graph_def = tf.GraphDef()
+                graph_def.ParseFromString(f.read())
+                tf.import_graph_def(graph_def, input_map=input_map, name='')
+        else:
+            PYTHON_LOGGER.info('Model directory: %s' % model_exp)
+            meta_file, ckpt_file = self.get_model_filename(model_exp)
+
+            PYTHON_LOGGER.info('Metagraph file: %s' % meta_file)
+            PYTHON_LOGGER.info('Checkpoint file: %s' % ckpt_file)
+
+            saver = tf.train.import_meta_graph(os.path.join(model_exp, meta_file), input_map=input_map)
+            saver.restore(tf.get_default_session(), os.path.join(model_exp, ckpt_file))
+
+    @staticmethod
+    def get_model_filename(model_dir):
+        """
+        Get the model file name
+        :param model_dir: (String) Absolute path to the model
+        :return: (string) Model name
+        """
+        ckpt = tf.train.get_checkpoint_state(model_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_file = os.path.basename(ckpt.model_checkpoint_path)
+            meta_file = ckpt_file + '.meta'
+            return meta_file, ckpt_file
+        else:
+            raise ValueError('No checkpoint file found in the model directory (%s)' % model_dir)
 
 
 if __name__ == "__main__":
 
+    # Read a image
+    img1 = cv2.imread("demo/testmoi.png")
+    img2 = cv2.imread("demo/testmoi2.png")
+    img1 = cv2.resize(img1, (600, 600))
+    img2 = cv2.resize(img2, (600, 600))
+    tow_images = np.concatenate((img1, img2), axis=0)
+    cv2.imshow("main", tow_images)
+    cv2.waitKey(0)
     image_translation = ImageTranslation("human_to_cat_128/128", 128)
-    output = image_translation.translate(cv2.imread("demo/testmoi.jpg"))
-    pass
+    PYTHON_LOGGER.info("Start translation")
+    # Translate to a cat image
+    output_image = image_translation.translate(img1)
+    cv2.imshow("main", output_image)
+    cv2.waitKey(0)
+    output_image = image_translation.translate(img2)
+    cv2.imshow("main", output_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
